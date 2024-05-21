4863184b6ae13d02a22878caeda84954f73d71fa
Ghostopheles <ghost@kivatech.io>
2024-05-21 03:14:54 -0500

    Add AceLocale and localization

---
+++ Verity/Localization/enUS.lua
@@ -0,0 +1,12 @@
+local AceLocale = LibStub("AceLocale-3.0");
+local L = AceLocale:NewLocale("Verity", "enUS", true);
+
+-- BEGIN LOCALIZATION
+
+L["ADDON_TITLE"] = "Verity";
+
+L["GAME_WINDOW_TITLE"] = "Verity";
+
+-- END LOCALIZATION
+
+Verity.Strings = AceLocale:GetLocale("Verity", false);
